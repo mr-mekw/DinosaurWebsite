49b68c70b444d26484103db87c38822f8e24a060
Colton Murray <mrmekw@gmail.com>
2026-04-01 16:49:30 -0500

    4/1 add cursors

--- Script.js
+++ Script.js
@@ -1,5 +1,10 @@
+/* 
+something isn't working here - this only works once even though it should be called
+every time the button is clicked... I don't think adding refresh every few seconds
+would be a good thing to do though...
+*/
 function startFunction(){
-    if (!(document.getElementById('credit-panel').style.display='none')) {
+    if (document.getElementById('credit-panel').style.display='flex') {
         document.getElementById('credit-panel').style.display='none';
     } else {
         document.getElementById('credit-panel').style.display='flex';
