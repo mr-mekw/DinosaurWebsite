330c81a13a717bdb1c27b20e8605400f98deee64
Colton Murray <mrmekw@gmail.com>
2026-04-01 17:37:17 -0500

    4/1 js stuff & icon

--- Script.js
+++ Script.js
@@ -2,11 +2,20 @@
 something isn't working here - this only works once even though it should be called
 every time the button is clicked... I don't think adding refresh every few seconds
 would be a good thing to do though...
+Neither of these work, but I want them to both work every time you click the buttons
 */
-function startFunction(){
+function creditFunction(){
     if (document.getElementById('credit-panel').style.display='flex') {
         document.getElementById('credit-panel').style.display='none';
     } else {
         document.getElementById('credit-panel').style.display='flex';
+    }
+}
+
+function startFunction(){
+    if (document.getElementById('game-container').style.display='flex'){
+        document.getElementById('game-container').style.display='none';
+    } else {
+        document.getElementById('game-container').style.display='flex';
     }
 }
