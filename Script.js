991e6208ea8ba9d15e95ca8783904f0da78b00dd
Colton Murray <mrmekw@gmail.com>
2026-04-01 13:58:29 -0500

    4/1/26
    lil additions all around

---
+++ Script.js
@@ -0,0 +1,7 @@
+function startFunction(){
+    if (!(document.getElementById('credit-panel').style.display='none')) {
+        document.getElementById('credit-panel').style.display='none';
+    } else {
+        document.getElementById('credit-panel').style.display='flex';
+    }
+}
