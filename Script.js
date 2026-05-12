5e1cd0ddf75302ef9d5a1fb8b87e9a4faed4ab74
Colton Murray <mrmekw@gmail.com>
2026-05-12 00:37:25 -0500

    5/12 @ 00:37

--- Script.js
+++ Script.js
@@ -1,14 +1,22 @@
 /* 
-something isn't working here - this only works once even though it should be called
+something isn't working here - these only works once even though it should be called
 every time the button is clicked... I don't think adding refresh every few seconds
 would be a good thing to do though...
-Neither of these work, but I want them to both work every time you click the buttons
+Neither of these work*, but I want them to both work every time you click the buttons
+
+
+I can either do <details> (for toggling if something shows or not, do this for the scoreboard)
+  or just <a href> link to a different html (should do that for the actual game screen)
 */
+
+/*
 function creditFunction(){
     if (document.getElementById('credit-panel').style.display='flex') {
         document.getElementById('credit-panel').style.display='none';
+        document.getElementById('credit-panel').appendChild;
     } else {
         document.getElementById('credit-panel').style.display='flex';
+        document.getElementById('credit-panel').appendChild;
     }
 }
 
@@ -19,3 +27,5 @@ function startFunction(){
         document.getElementById('game-container').style.display='flex';
     }
 }
+OUT OF USE AS OF 08/4/2026
+*/
